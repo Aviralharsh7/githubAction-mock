2245bcb267752f221da2b32153223260ef8554ac
Aviralharsh7 <Harshy2026@gmail.com>
2023-12-18 19:21:02 +0530

    /debug 13

--- utils/scripts/imageProcessing.js
+++ utils/scripts/imageProcessing.js
@@ -1,5 +1,6 @@
-// mockup_script.js
-
 const fs = require('fs');
+
+// Create or modify a file
+fs.writeFileSync('example.txt', 'This is a change made by the mock script.');
 
 console.log('Mockup script is running at', new Date());
