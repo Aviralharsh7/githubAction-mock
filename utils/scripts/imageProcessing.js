a848addab47ddf9a4562e294e3fad9d324b77cfa
Aviralharsh7 <Harshy2026@gmail.com>
2023-12-19 18:33:52 +0530

    / debug 20

--- utils/scripts/imageProcessing.js
+++ utils/scripts/imageProcessing.js
@@ -1,6 +1,27 @@
 const fs = require('fs');
+const path = require('path');
 
-// Create or modify a file
-fs.writeFileSync('example.txt', 'This is a change made by the mock script.');
+const folderPath = 'assets';
 
-console.log('Mockup script is running at', new Date());
+fs.readdir(folderPath, (err, files) => {
+  if (err) {
+    console.error('Error reading directory:', err);
+    return;
+  }
+
+  files.forEach(file => {
+    const filePath = path.join(folderPath, file);
+
+    // Check if the file is a PNG file
+    if (path.extname(filePath).toLowerCase() === '.png') {
+      // Delete the file
+      fs.unlink(filePath, err => {
+        if (err) {
+          console.error('Error deleting file:', filePath, err);
+        } else {
+          console.log('Deleted file:', filePath);
+        }
+      });
+    }
+  });
+});
